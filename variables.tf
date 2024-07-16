97f2979c212192b741651d58b185b998e78b128d
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 10:20:39 -0300

    ci: remove az credentials from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "az_subscription_id" {
-  type        = string
-  description = "azure subscription id"
-}
-
-variable "az_tenant_id" {
-  type        = string
-  description = "azure tenant id"
-}
-
-variable "az_client_id" {
-  type        = string
-  description = "azure client id"
-}
-
-variable "az_client_secret" {
-  type        = string
-  description = "azure clinet secret"
-}
-
 variable "resource_group_name" {
   type        = string
   description = "resource group name"
