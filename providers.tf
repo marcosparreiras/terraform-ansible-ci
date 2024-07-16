50d5f3de8bfbacd14c7f15770a97e69bfe5b92d9
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 10:33:13 -0300

    fix: ci

--- providers.tf
+++ providers.tf
@@ -18,8 +18,4 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id = var.az_subscription_id
-  tenant_id       = var.az_tenant_id
-  client_id       = var.az_client_id
-  client_secret   = var.az_client_secret
 }
