5936a2602a31ab821772dc6b97a89d40db9bd6f5
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 10:26:54 -0300

    fix: another env bug with terraform github workflow

--- providers.tf
+++ providers.tf
@@ -18,4 +18,8 @@ provider "azurerm" {
   features {
 
   }
+  subscription_id = var.az_subscription_id
+  tenant_id       = var.az_tenant_id
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
 }
